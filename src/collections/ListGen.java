e3dbaa61c549d8b2ea42c7c908b784437745fc97
venkateshkandula <venkateshkandula7@gmail.com>
2017-12-30 22:55:37 -0600

    updated

--- src/collections/ListGen.java
+++ src/collections/ListGen.java
@@ -3,8 +3,9 @@ import java.util.ArrayList;
 
 @FunctionalInterface
 interface ListGen<T> {
-	    Customer compareGenericMethod(Customer a,Object obj);
-	 public static ArrayList<Customer> compareGenericMethod1(ArrayList<Customer> a, char c) {
+	    Customer compareGenericMethod(Customer a,Object obj);//only one abstract method
+	//functional interface can contain any number of static and default methods:
+	    public static ArrayList<Customer> compareGenericMethod1(ArrayList<Customer> a, char c) {
 			ArrayList<Customer> alc=new ArrayList<Customer>();
 			for (Customer customer : a) {
 				if(customer.getName().charAt(0)==c){
